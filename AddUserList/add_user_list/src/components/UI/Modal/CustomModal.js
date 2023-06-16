f5f430bb64a9ef16e2e44331b824827dda8ec380
nitish200899 <nitishkesarwani20@gmail.com>
2023-06-17 00:00:33 +0530

    Fragments and Portals added to modal

--- AddUserList/add_user_list/src/components/UI/Modal/CustomModal.js
+++ AddUserList/add_user_list/src/components/UI/Modal/CustomModal.js
@@ -1,30 +1,47 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Card from "../Card/Card";
 import styles from "./CustomModal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onClose} />;
+};
+
+const Modal = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <button type="button" onClick={props.onClose} className={styles.button}>
+          Okay
+        </button>
+      </footer>
+    </Card>
+  );
+};
+
 const CustomModal = (props) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onClose} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <button
-            type="button"
-            onClick={props.onClose}
-            className={styles.button}
-          >
-            Okay
-          </button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onClose={props.onClose}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 };
 
